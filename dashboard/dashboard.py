87193285d47bb6ced808cbc7c539764bf22cf169
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2023-10-26 09:22:00 +0700

    changed the csv url

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 sns.set_style('darkgrid')
 
 # Load Data
-students = pd.read_csv('../dashboard/students.csv')
+students = pd.read_csv('https://github.com/nurkholiqaganihafid/Analysis_Student_Dropout_Rates/blob/main/dashboard/students.csv')
 
 def display_dataframe(option):
     selected_student = students[students['status'].isin(option)]
